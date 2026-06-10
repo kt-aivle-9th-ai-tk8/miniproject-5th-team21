37cb74e86b76074d23c058b86e4fb6a9b7f6aafb
IT-Gentleman <gentleman@kakao.com>
2026-06-10 13:20:26 +0900

    chore: remove unused import and add comments

--- src/main/java/com/aivle/bookapp/repository/BookRepository.java
+++ src/main/java/com/aivle/bookapp/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package com.aivle.bookapp.repository;
 
 import com.aivle.bookapp.domain.Book;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -22,6 +21,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
         return findByCategoryAndTitleContainingOrCategoryAndAuthorContaining(category, keyword, category, keyword);
     }
 
-    List<Book> findByTitleContainingOrAuthorContaining(String title, String author); // 직접 미사용
+    // 아래 메서드는 직접 호출하여 사용하는 것을 권장하지 않음 (과도하게 복잡함) --> 위에 `default`로 정의해놓은 메서드 사용
+    List<Book> findByTitleContainingOrAuthorContaining(String title, String author);
     List<Book> findByCategoryAndTitleContainingOrCategoryAndAuthorContaining(String category1, String title, String category2, String author);
 }
