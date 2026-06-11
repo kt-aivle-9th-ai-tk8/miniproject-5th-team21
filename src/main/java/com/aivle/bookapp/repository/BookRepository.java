fab32acd49fe1a956654d67aab601f8ccb3511bc
IT-Gentleman <gentleman@kakao.com>
2026-06-11 09:24:39 +0900

    chore: add repository annotation for clarification

--- src/main/java/com/aivle/bookapp/repository/BookRepository.java
+++ src/main/java/com/aivle/bookapp/repository/BookRepository.java
@@ -2,9 +2,11 @@ package com.aivle.bookapp.repository;
 
 import com.aivle.bookapp.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     // category 비명시
