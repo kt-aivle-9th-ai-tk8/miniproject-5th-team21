3353eae4ba562e6cbf7b142135aff390280e7e28
parkjiyeon <cory216@naver.com>
2026-06-11 13:34:14 +0900

    컨트롤러 및 리드미 복구 완료

--- src/main/java/com/aivle/bookapp/controller/BookController.java
+++ src/main/java/com/aivle/bookapp/controller/BookController.java
@@ -54,10 +54,10 @@ public class BookController {
     }
 
     // AI 도서 표지 수정
-    @PatchMapping("/books/{id}/cover")
+    /*@PatchMapping("/books/{id}/cover")
     public ResponseEntity<Book> aiBookCover(@PathVariable Long id, @RequestBody String coverImageUrl) {
         Book aiCover = bookService.oooooooo(id, coverImageUrl); // bookService 메소드명 정해지면 수정
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(aiCover);
-    }
+    }*/
 }
