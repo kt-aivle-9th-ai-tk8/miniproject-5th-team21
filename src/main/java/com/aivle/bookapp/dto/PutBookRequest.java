c0ff2ba5a1b6bf01eeed578f9d7ee0251d9c24e7
IT-Gentleman <gentleman@kakao.com>
2026-06-11 10:26:20 +0900

    fix: add missed Not Blank validation to category

--- src/main/java/com/aivle/bookapp/dto/PutBookRequest.java
+++ src/main/java/com/aivle/bookapp/dto/PutBookRequest.java
@@ -11,6 +11,7 @@ public record PutBookRequest(
         @NotBlank(message="도서 본문은 필수적으로 작성되어야 합니다.")
         String content,
         String coverImageUrl,
+        @NotBlank(message="도서 분류는 필수적으로 작성되어야 합니다.")
         String category,
         // createdAt(생성일자)는 수정되지 않으므로 요청에 포함하지 않습니다.
         @NotBlank(message="JPA Auditing이 적용되어있지 않으므로, 수정일자를 주입해주십시오.")
