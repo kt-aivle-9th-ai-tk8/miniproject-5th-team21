27080b282769b9e402c4f5735b0d3aaf1f091889
IT-Gentleman <gentleman@kakao.com>
2026-06-11 10:20:46 +0900

    fix: make `coverImageUrl` lowerCamelCase

--- src/main/java/com/aivle/bookapp/dto/UpdateBookCoverImageUrlCommand.java
+++ src/main/java/com/aivle/bookapp/dto/UpdateBookCoverImageUrlCommand.java
@@ -2,6 +2,6 @@ package com.aivle.bookapp.dto;
 
 // 향후 book patch 범용 메서드 작성 시, UpdateBookCommand로 통폐합
 public record UpdateBookCoverImageUrlCommand(
-        String CoverImageUrl
+        String coverImageUrl
 ) {
 }
